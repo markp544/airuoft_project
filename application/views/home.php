a96ababa1faede3faa0caae8e242bf0e38830d1b
bas13 <basem13@gmail.com>
2013-07-20 13:51:54 -0400

    Added the home page.

---
+++ application/views/home.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Main Page</title>
+</head>
+<body>
+	<h1>Welcome to AirUofT</h1>
+	<br/>
+	<h4>Please fill out the info. below: </h4>
+	<form action="">
+		<select name="campus">
+			<option value="stg">St.George</option>
+			<option value="utm">UTM</option>
+		</select>
+		<br/><br/>
+		<input type="submit" value="submit">
+	</form>
+	<?php echo '<h1>hello</h1>';?>
+</body>
+</html>
+

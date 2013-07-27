a62a224a2c0d253e158d4f2a094ee85d64ebeda2
bas13 <basem13@gmail.com>
2013-07-27 11:35:14 -0400

    Adding seatsview2.

---
+++ application/views/seatview2.php
@@ -0,0 +1,6 @@
+<div id="planeseat" >
+<input type="button" id="seat1" class="<?=$seat1?>"/>
+<input type="button" id="seat2" class="<?=$seat2?>"/>
+<input type="button" id="seat3" class="<?=$seat3?>"/>
+<p id="test">hi</p>
+</div>
